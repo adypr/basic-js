e73f57eeab41cfba2b953cbdb1358430b0b5adf7
Adypr <workady2015@gmail.com>
2022-04-30 11:34:56 +0300

    feat: complete what season

--- src/what-season.js
+++ src/what-season.js
@@ -12,11 +12,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
-  
   try {
-    if ((date instanceof Date) === false) throw new Error();
+    if (typeof date === 'undefined') return  'Unable to determine the time of year!';
+    if ((date instanceof Date) && Object.getOwnPropertyNames(date).length > 0) throw new Error();
     
     const month = date.getMonth();
+    
    
     if (month <= 1 || month === 11) return 'winter';
     if (month <= 4) return 'spring';
@@ -24,8 +25,6 @@ function getSeason(date) {
     return 'autumn';
   }
   catch {
-
-    if (typeof date === 'undefined') return  'Unable to determine the time of year!';
     
     throw new Error('Invalid date!');
   }
@@ -35,3 +34,33 @@ module.exports = {
   getSeason
 };
 
+const deeperFakeDate = {
+  toString() {
+      return Date.prototype.toString.call(new Date());
+  },
+  getMonth() {
+      return Date.prototype.getMonth.call(new Date());
+  },
+  getFullYear() {
+      return Date.prototype.getFullYear.call(new Date(1994, 1, 2, 3, 4, 5));
+  },
+  getDate() {
+      return Date.prototype.getDate.call(new Date(2020, 0, 3, 4, 5, 6));
+  },
+  getHours() {
+      return Date.prototype.getHours.call(new Date(1978, 2, 4, 5, 6, 7));
+  },
+  getMinutes() {
+      return Date.prototype.getMinutes.call(new Date(202, 3, 5, 6, 7, 8));
+  },
+  getSeconds() {
+      return Date.prototype.getSeconds.call(new Date(1, 4, 6, 7, 8, 9));
+  },
+  getMilliseconds() {
+      return Date.prototype.getMilliseconds.call(new Date(2019, 7, 8, 9, 10, 11));
+  },
+  getDay() {
+      return Date.prototype.getDay.call(new Date(1812, 8, 9, 10, 11, 12));
+  },
+  [Symbol.toStringTag]: 'Date'
+};
