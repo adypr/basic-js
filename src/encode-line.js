631fba3bb116fcecdcd7f6a438b4c86443746fbd
Adypr <workady2015@gmail.com>
2022-05-02 22:38:16 +0300

    feat: add encode-line

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,11 +10,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  return str.split('')
+            .reduce((acc, elem) => {
+              if (acc[acc.length - 1] && acc[acc.length - 1][1] === elem) acc[acc.length - 1][0] += 1;
+              else acc.push([1, elem]);
+              return acc;
+            }, [])
+            .map((elem) => {
+              return elem[0] === 1 ? elem[1] : `${elem[0]}${elem[1]}`;
+            })
+            .join('');
 }
 
 module.exports = {
   encodeLine
 };
+
+// My desision for all includes
+
+// function encodeLine(str) {
+//   let arr = [];
+//   for (let i = 0; i < str.length; i += 1) {
+//     let regexp = new RegExp(`${str[i]}`, 'gi');
+//     let num = str.match(regexp).length;
+//     if (num === 1) num = '';
+//     arr.push(`${num}${str[i]}`)
+//   }
+//   return Array.from((new Set(arr))).join('');
+// }
+
+console.log(encodeLine('aabbbca'))
