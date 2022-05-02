a3e4d7233f19387508a17ee040117451940113b6
Adypr <workady2015@gmail.com>
2022-05-02 18:52:05 +0300

    feat: add simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,28 +5,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [], 
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    this.chain.push(value);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+    if (typeof position === 'number' && position % 1 === 0 && (position - 1 >= 0 && position < this.chain.length)) {
+      this.chain.splice(position - 1, 1);
+    }
+    else {
+      this.chain = [];
+      throw new Error("You can\'t remove incorrect link!");
+    } 
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let str = `( ${this.chain[0]} )`;
+    for (let i = 1; i < this.chain.length; i +=1) {
+      str += '~~';
+      str += `( ${this.chain[i]} )`;
+    }
+    this.chain = []
+    return str;
   }
 };
 
 module.exports = {
   chainMaker
 };
+
+//console.log(chainMaker.addLink(1).addLink(2).reverseChain().addLink(3).finishChain());
+//console.log(chainMaker.addLink('GHI').addLink(null).reverseChain().addLink(333).reverseChain().reverseChain().addLink(0).reverseChain().reverseChain().addLink('GHI').finishChain())
+//chainMaker.addLink(5).addLink(7);
+//chainMaker.addLink(5).addLink(7);
+
+// console.log(chainMaker.addLink(null).addLink(false).addLink(22)
+//                       .reverseChain().reverseChain()
+//                       .removeLink(2)
+//                       .reverseChain().reverseChain()
+//                       .addLink({ 0: 'first', 1: 'second', 'length': 2 })
+//                       .reverseChain().addLink('DEF').finishChain());
